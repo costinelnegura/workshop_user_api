aab5073ce2d61090fecaa8fcf6f9484358a63388
costinelnegura <pigeons-conduit0n@icloud.com>
2024-03-22 10:13:24 +0000

    updated application.yaml database connection to use mysql instance from docker

--- src/main/java/uk/co/negura/workshop_users_api/security/SecurityConfig.java
+++ src/main/java/uk/co/negura/workshop_users_api/security/SecurityConfig.java
@@ -70,8 +70,8 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST,"/api/v1/auth/login").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/auth/validate").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/v1/users/create").hasAuthority("USER_DETAILS_WRITE")
-                        .requestMatchers(HttpMethod.GET, "/api/v1/users/{ID}").authenticated()
-                        .requestMatchers(HttpMethod.PATCH, "/api/v1/users/{ID}").authenticated()
+                        .requestMatchers(HttpMethod.GET, "/api/v1/users/{ID}").hasAuthority("USER_DETAILS_READ")
+                        .requestMatchers(HttpMethod.PATCH, "/api/v1/users/{ID}").hasAuthority("USER_DETAILS_WRITE")
                         .requestMatchers(HttpMethod.DELETE, "/api/v1/users/{ID}").hasAuthority("USER_DETAILS_DELETE")
                         .anyRequest().authenticated());
         http
